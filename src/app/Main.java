c8042764957e841a2f8af14972f05b7926ea45da
Vítor Dantas Pequeno <vitor.dantas.pequeno@ccc.ufcg.edu.br>
2025-11-19 22:34:34 -0300

    feat: Implementa menu principal e loop de interacao

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,19 +3,55 @@ package app;
 // 1. Importamos as classes que criamos no outro pacote
 import domain.Disciplina;
 import domain.Tarefa;
-import java.time.LocalDate; // Precisamos importar o LocalDate do Java
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         
-        Disciplina java = new Disciplina("Java", "Prof. Aristóteles");
-        Tarefa estudar = new Tarefa("Estudar POO", LocalDate.of(2025, 11, 18));
+        Scanner scanner = new Scanner(System.in);
+        boolean executando = true;
         
-        java.adicionarTarefa(estudar);
-        System.out.println("Nome da disciplina: " + java.getNome());
-        System.out.println("Título da tarefa: " + estudar.getTitulo());
-        System.out.println("Título da tarefa: " + java.getTarefas().get(0).getTitulo());
-        System.out.println("Prazo da tarefa: " + java.getTarefas().get(0).getPrazo());
+        while (executando)  {
+            menu();
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                
+                case 1 ->
+                    System.out.println(">> (em breve) Cadastrar disciplina");
+
+                case 2 ->
+                    System.out.println(">> (em breve) Cadastrar tarefa");
+            
+                case 3 ->
+                    System.out.println(">> (em breve) Listar tudo");
+
+                case 0 -> {
+                    System.out.println("Saindo...");
+                    executando = false;
+                }
+                default ->
+                    System.out.println("Opção inválida!");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
 
     }
+    
+    public static void menu() {
+
+        String menu = """
+                ---- STUDY TRACKER ----
+                1. Cadastrar disciplina
+                2. Cadastrar tarefa
+                3. Listar tudo
+                0. Sair
+                Escolha uma opção:\s""";
+        System.out.print(menu);
+
+    }
+
 }
